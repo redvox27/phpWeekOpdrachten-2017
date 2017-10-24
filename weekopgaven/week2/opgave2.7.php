de2377b8f3fdb7847e9dfe46b641bbe83c06b76b
redvox27 <v.t.luder@st.hanze.nl>
2017-10-24 14:43:53 +0200

    opdr 2.7 af

--- weekopgaven/week2/opgave2.7.php
+++ weekopgaven/week2/opgave2.7.php
@@ -1,7 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Gebruiker
- * Date: 23-10-2017
- * Time: 15:17
- */
+$weatherArray = [];
+$drogeDagen = 0;
+$totaalAantalNeerslag = 0;
+
+
+# fill the array
+for ($i = 0; $i < 20; $i++){
+    $random = rand(0, 100);
+    $weatherArray[$i] = $random;
+}
+
+for ($i = 0; $i < count($weatherArray); $i++){
+    $totaalAantalNeerslag += $weatherArray[$i];
+
+    if ($weatherArray[$i] == 0){
+        $drogeDagen += 1;
+    }
+}
+echo $drogeDagen . "</br>";
+echo $totaalAantalNeerslag . "</br>";
+
+?>
